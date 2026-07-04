ce7b83c15dcb3db135fd78191e9eeaaa769ddc81
Methewdev <envidevelopment@gmail.com>
2026-07-04 11:20:07 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+MODEL_NAME = "envidevelopment/livin-emotion-indobert"
+
+USE_LOCAL_MODEL = False
+
+MAX_LENGTH = 128
+
+EMOTION_LABELS = [
+    "Frustrasi",
+    "Netral",
+    "Sedih",
+    "Senang"
+]
