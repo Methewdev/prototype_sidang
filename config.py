c9dc296a47c02d80c2fb6f14679f8c0a315e7a1d
Methewdev <envidevelopment@gmail.com>
2026-07-04 11:45:03 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,25 @@
-MODEL_NAME = "envidevelopment/livin-emotion-indobert"
+from pathlib import Path
 
+ROOT = Path(__file__).resolve().parent
+
+APP_TITLE = "Livin Emotion Analysis"
+APP_ICON = "📊"
+
+ASSET_DIR = ROOT / "assets"
+
+CSS_FILE = ASSET_DIR / "style.css"
+LOGO = ASSET_DIR / "logo.png"
+SLANG_FILE = ASSET_DIR / "new_kamus_alay.csv"
+
+MODEL_NAME = "envidevelopment/livin-emotion-indobert"
 USE_LOCAL_MODEL = False
 
+MODEL_PATH = ROOT / "models"
+DATA_PATH = ROOT / "data"
+
 MAX_LENGTH = 128
+RANDOM_STATE = 42
+N_CLUSTER = 3
 
 EMOTION_LABELS = [
     "Frustrasi",
